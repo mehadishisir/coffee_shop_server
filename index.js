4b9642da540b9b3ad402e14185cfa0c012da325f
mehadi <mehadishisir@gmail.com>
2026-02-09 11:24:30 +0600

    add details

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.toblij9.mongodb.net/?appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -21,42 +20,48 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // create collection
+
     const coffeecollection = client.db("coffeeDB").collection("coffees");
 
-    // c operations
+    // C – create
     app.post("/coffees", async (req, res) => {
       const newCoffee = req.body;
-      console.log(newCoffee);
       const result = await coffeecollection.insertOne(newCoffee);
       res.send(result);
     });
-    // r operations
+    // details
+    app.get("/coffees/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeecollection.findOne(query);
+      res.send(result);
+    });
+
+    // R – read
     app.get("/coffees", async (req, res) => {
       const result = await coffeecollection.find().toArray();
       res.send(result);
     });
 
-    // d operations
+    // D – delete
     app.delete("/coffees/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeecollection.deleteOne(query);
       res.send(result);
     });
-    // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+  } catch (error) {
+    console.log(error);
   }
 }
-run().catch(console.dir);
+
+run();
 
 app.get("/", (req, res) => {
   res.send("coffee server is ready");
