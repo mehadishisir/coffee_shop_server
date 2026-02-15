59ad9e14ade713ae617d6362077299bdf378060e
mehadi <mehadishisir@gmail.com>
2026-02-15 11:03:27 +0600

    add delete operation for users

--- index.js
+++ index.js
@@ -75,6 +75,12 @@ async function run() {
       const result = await userCollection.insertOne(newUser);
       res.send(result);
     });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
