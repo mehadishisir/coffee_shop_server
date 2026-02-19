790e1260fc816873e68fce61b50d4f40a71803ef
mehadi <mehadishisir@gmail.com>
2026-02-19 19:53:40 +0600

    add patch operation

--- index.js
+++ index.js
@@ -75,6 +75,17 @@ async function run() {
       const result = await userCollection.insertOne(newUser);
       res.send(result);
     });
+    app.patch("/users", async (req, res) => {
+      const { email, lastSignInTime } = req.body;
+      const filter = { email: email };
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: lastSignInTime,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
